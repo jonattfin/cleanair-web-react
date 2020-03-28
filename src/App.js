66c093b7150204525fdeb681704a2eee6c22f807
jonattfin <jonattfin@gmail.com>
2020-03-28 22:25:20 +0200

    initial version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex-container">
+        <h1>Clean AIR</h1>
+    </div>
+  );
+}
+
+export default App;
